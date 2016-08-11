8f50598ee744bb7e2a1088d0c20849b04a12eafd
s.melnikov <s.melnikov@bpcbt.com>
2016-08-11 20:09:48 +0300

    add styling pages.

--- app/app.component.ts
+++ app/app.component.ts
@@ -15,6 +15,7 @@ import {HeroesComponent} from './heroes.component';
     </nav>
     <router-outlet></router-outlet>
   `,
+  styleUrls:['app/app.component.css'],
   directives: [ROUTER_DIRECTIVES],
   providers: [
     HeroService
--- app/hero-detail.component.js
+++ app/hero-detail.component.js
@@ -30,6 +30,7 @@ var HeroDetailComponent = (function () {
         core_1.Component({
             selector: 'my-hero-detail',
             templateUrl: 'app/hero-detail.component.html',
+            styleUrls: ['app/hero-detail.component.css']
         }), 
         __metadata('design:paramtypes', [hero_service_1.HeroService, router_1.ActivatedRoute])
     ], HeroDetailComponent);
