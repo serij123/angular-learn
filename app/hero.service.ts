0a6fad715dd245ecaf76511e0259a7ce801f8407
s.melnikov <s.melnikov@bpcbt.com>
2016-08-12 21:09:04 +0300

    add normal but in-memory heroes REST service.

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,17 +1,64 @@
-import {Injectable} from '@angular/core';
-import {HEROES} from './mock-heroes';
+import { Injectable }    from '@angular/core';
+import { Http,Headers }     from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+import { Hero }     from './hero';
 
 @Injectable()
 export class HeroService{
+	private heroesUrl = 'app/heroes';
+
+	constructor(private http : Http) {
+	}
+
 	getHeroes() {
-		/*for simulate slow connection:
-		return new Promise<Hero[]>(resolve =>
-			setTimeout(() => resolve(HEROES), 2000)
-		);*/
-		return Promise.resolve(HEROES);
+		return this.http.get(this.heroesUrl)
+				.toPromise()
+				.then(response => response.json().data as Hero[])
+				.catch(this.handleError);
 	}
 
 	getHero(id: number) {
 		return this.getHeroes().then(heroes => heroes.find(hero=>hero.id === id));
+	}
+
+	save(hero: Hero): Promise<Hero>  {
+	  if (hero.id) {
+	    return this.put(hero);
+	  }
+	  return this.post(hero);
+	}
+
+
+	private post(hero : Hero) : Promise<Hero> {
+		let headers = new Headers({'Content-Type':'application/json'});
+		return this.http.post(this.heroesUrl, JSON.stringify(hero), {headers:headers})
+			.toPromise().then(res => res.json().data)
+			.catch(this.handleError);
+	}
+
+	private put(hero : Hero) : Promise<Hero> {
+		let headers = new Headers({'Content-Type':'application/json'});
+		let url = `${this.heroesUrl}/${hero.id}`;
+
+		return this.http.put(this.heroesUrl, JSON.stringify(hero), {headers:headers})
+			.toPromise().then(() => hero)
+			.catch(this.handleError);
+	}
+	private delete(hero : Hero) : Promise<Hero> {
+		let headers = new Headers({'Content-Type':'application/json'});
+		let url = `${this.heroesUrl}/${hero.id}`;
+
+		return this.http.delete(url, {headers:headers})
+			.toPromise()
+			.then(() => hero)
+			.catch(this.handleError);
+	}
+
+
+	private handleError(error : any) {
+		console.error('An error occured ', error);
+		return Promise.reject(error.message || error);
 	}
 }
